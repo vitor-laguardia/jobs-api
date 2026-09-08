f02d0976f2081501ce13c8bc9a6d5c157b788263
vitor-laguardia <vitor.laguardia@hotmail.com>
2026-09-08 10:25:02 -0300

    feat: create User Repository (in memory)

---
+++ internal/user/in_memory_user_repository.go
@@ -0,0 +1,24 @@
+package user
+
+type UserRepository struct {
+	users map[string]User
+}
+
+func NewInMemoryRepository() *UserRepository {
+	return &UserRepository{users: make(map[string]User)}
+}
+
+func (ur *UserRepository) GetByID(userID string) (User, error) {
+	user, exists := ur.users[userID]
+
+	if !exists {
+		return User{}, ErrNotFound
+	}
+
+	return user, nil
+}
+
+func (ur *UserRepository) Create(user User) User {
+	ur.users[user.ID] = user
+	return user
+}
